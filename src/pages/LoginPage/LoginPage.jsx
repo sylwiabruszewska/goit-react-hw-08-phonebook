f528ec61bea3e45498d12588426bbff91f0d0af1
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-10-03 20:50:33 +0200

    update Login and Register pages

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,15 +1,31 @@
-import { LoginForm } from 'components';
+import { AuthForm } from 'components';
 
 import { Helmet } from 'react-helmet';
 
+import styled from 'styled-components';
+import photo from 'images/auth.png';
+import { Container, Content } from 'components';
+
+const StyledImg = styled.img`
+  width: auto;
+  height: 500px;
+  object-fit: contain;
+`;
+
 const LoginPage = () => {
   return (
-    <div>
+    <>
       <Helmet>
         <title>Phonebook App - Login</title>
       </Helmet>
-      <LoginForm />
-    </div>
+
+      <Content>
+        <Container>
+          <AuthForm />
+        </Container>
+        <StyledImg src={photo} />
+      </Content>
+    </>
   );
 };
 
--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,15 +1,31 @@
-import { RegisterForm } from 'components';
+import { AuthForm } from 'components';
 
 import { Helmet } from 'react-helmet';
 
+import styled from 'styled-components';
+import photo from 'images/auth.png';
+import { Container, Content } from 'components';
+
+const StyledImg = styled.img`
+  width: auto;
+  height: 500px;
+  object-fit: contain;
+`;
+
 const RegisterPage = () => {
   return (
-    <div>
+    <>
       <Helmet>
         <title>Phonebook App - Register</title>
       </Helmet>
-      <RegisterForm />
-    </div>
+
+      <Content>
+        <Container>
+          <AuthForm />
+        </Container>
+        <StyledImg src={photo} />
+      </Content>
+    </>
   );
 };
 
