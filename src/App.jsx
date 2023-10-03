85089d155f267e72a7c21610c3af6c4f3c5a086c
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-10-03 21:30:43 +0200

    clean the code, add Loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
-import { SharedLayout, PrivateRoute, RestrictedRoute } from './components';
-
 import { useDispatch } from 'react-redux';
+import { useEffect, lazy } from 'react';
+
+import {
+  SharedLayout,
+  PrivateRoute,
+  RestrictedRoute,
+  Loader,
+} from './components';
 import { refreshUser } from 'redux/auth/operations';
 import { useAuth } from 'hooks';
-
-import { useEffect, lazy } from 'react';
 
 const Home = lazy(() => import('pages/Home/Home'));
 const Contacts = lazy(() => import('pages/Contacts/Contacts'));
@@ -22,7 +26,7 @@ export const App = () => {
   }, [dispatch]);
 
   return isRefreshing ? (
-    <b>Refreshing user...</b>
+    <Loader />
   ) : (
     <>
       <Routes>
