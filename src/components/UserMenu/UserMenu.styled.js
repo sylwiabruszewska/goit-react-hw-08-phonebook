0e2cca8f4c306133aa7aec1fc81518964e712317
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-10-03 22:23:07 +0200

    change styles

--- src/components/UserMenu/UserMenu.styled.js
+++ src/components/UserMenu/UserMenu.styled.js
@@ -14,10 +14,10 @@ export const StyledSpan = styled.span`
 export const StyledBoldSpan = styled.span`
   font-size: 1.2rem;
   letter-spacing: 0.2rem;
-  color: #fb8e5d;
+  color: #fff;
   border-radius: 4px;
   display: inline-block;
-  background-color: #fff;
+  background-color: #fb8e5d;
   padding: 5px;
   margin-right: 0.5rem;
   box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.1);
