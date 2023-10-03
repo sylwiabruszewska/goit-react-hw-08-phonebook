ff708c0ef8626cfe0d20251b3a29149837db0ed2
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-10-03 20:37:56 +0200

    update Container styles

--- src/components/Container/Container.styled.jsx
+++ src/components/Container/Container.styled.jsx
@@ -2,20 +2,22 @@ import styled from 'styled-components';
 
 export const StyledContainer = styled.div`
   background-color: #fff;
-  margin: 20px auto;
-  padding: 20px;
+  padding: 30px;
   border-radius: 10px;
   color: #313131;
   box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
   min-width: 300px;
-  width: 90%;
+  width: fit-content;
 
-  @media (min-width: 768px) {
-    width: 70%;
+  /* @media (min-width: 768px) {
+    width: fit-content;
   }
 
   @media (min-width: 1200px) {
-    width: 50%;
-  }
+    width: fit-content;
+  } */
 `;
