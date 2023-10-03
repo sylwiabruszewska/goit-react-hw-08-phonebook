fb7591e01be2a4e51900a8877ce44378a7df8b55
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-10-03 21:50:52 +0200

    update NotFound page, add styles

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,14 +1,33 @@
 import { Helmet } from 'react-helmet';
+import { Link } from 'react-router-dom';
+
+// change img for 404
+import photo from 'images/auth.png';
+import { Content, Button } from 'components';
+import { StyledImg, StyledHeading, StyledText } from './NotFound.styled';
 
 const NotFound = () => {
   return (
-    <div>
+    <>
       <Helmet>
         <title>Phonebook App - Page not found</title>
       </Helmet>
-      <h2>404 - Page not found</h2>
-      <p>Sorry, the page you are looking for does not exist.</p>
-    </div>
+
+      <Content>
+        <div>
+          <StyledHeading>404 - Whoops! Page not found.</StyledHeading>
+          <StyledText>
+            Sorry, the page you are looking for does not exist.
+          </StyledText>
+
+          <Link to="/">
+            <Button type="button">Go Home</Button>
+          </Link>
+        </div>
+
+        <StyledImg src={photo} />
+      </Content>
+    </>
   );
 };
 
---
+++ src/pages/NotFound/NotFound.styled.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const StyledImg = styled.img`
+  width: auto;
+  height: 500px;
+  object-fit: contain;
+`;
+
+export const StyledHeading = styled.h2`
+  font-size: 2rem;
+  margin-bottom: 2rem;
+`;
+
+export const StyledText = styled.p`
+  font-size: 1.4rem;
+  margin-bottom: 1rem;
+`;
