7a6e1b0b1ba712a6c254ec431008bfe5daca605f
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-10-03 19:35:26 +0200

    add one component for auth form

---
+++ src/components/AuthForm/AuthForm.jsx
@@ -0,0 +1,81 @@
+import { useLocation } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { register, logIn } from 'redux/auth/operations';
+import { useState } from 'react';
+
+import { StyledForm, StyledFormTitle, StyledIcon } from './AuthForm.styled';
+import { Button, Input, InputField } from 'components';
+
+export const AuthForm = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const dispatch = useDispatch();
+  const location = useLocation().pathname;
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const form = event.currentTarget;
+
+    if (location === '/register') {
+      dispatch(register({ name, email, password }));
+    }
+
+    if (location === '/login') {
+      dispatch(logIn({ email, password }));
+    }
+
+    form.reset();
+  };
+
+  return (
+    <div>
+      <StyledForm onSubmit={handleSubmit}>
+        <StyledIcon />
+        <StyledFormTitle>
+          {location === '/login' ? 'Sign in' : 'Sign up'}
+        </StyledFormTitle>
+        {location === '/register' ? (
+          <InputField label="Name">
+            <Input
+              type="text"
+              name="name"
+              value={name}
+              placeholder="Name"
+              required
+              onChange={e => setName(e.target.value)}
+            />
+          </InputField>
+        ) : null}
+
+        <InputField label="Email">
+          <Input
+            type="email"
+            name="email"
+            value={email}
+            placeholder="Email"
+            required
+            onChange={e => setEmail(e.target.value)}
+          />
+        </InputField>
+        <InputField label="Password">
+          <Input
+            type="password"
+            name="password"
+            value={password}
+            placeholder="Password"
+            required
+            autocomplete="current-password"
+            onChange={e => setPassword(e.target.value)}
+          />
+        </InputField>
+        <Button type="submit">
+          {location === '/register'
+            ? `Sign Up and Stay Connected!`
+            : `Let's Get Contacted!`}
+        </Button>
+      </StyledForm>
+    </div>
+  );
+};
---
+++ src/components/AuthForm/AuthForm.styled.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import { BiSolidLockAlt } from 'react-icons/bi';
+
+export const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+`;
+
+export const StyledFormTitle = styled.h3`
+  font-size: 1.2rem;
+
+  color: #a06cd5;
+  font-weight: 500;
+`;
+
+export const StyledIcon = styled(BiSolidLockAlt)`
+  font-size: 2rem;
+  color: #a06cd5;
+`;
