23c5941ea65bcb766132043fbf8401dac313360b
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-10-04 23:37:35 +0200

    add Modal in SharedLayout

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
 
-import { Loader, Navigation } from 'components';
+import { Loader, Navigation, Modal } from 'components';
 import { Header } from './SharedLayout.styled';
+import { useSelector } from 'react-redux';
+import { selectModalOpen } from 'redux/modal/selectors';
 
 export const SharedLayout = () => {
+  const modalOpen = useSelector(selectModalOpen);
+
   return (
     <>
       <Header>
@@ -13,6 +17,7 @@ export const SharedLayout = () => {
       <Suspense fallback={<Loader />}>
         <Outlet />
       </Suspense>
+      {modalOpen && <Modal />}
     </>
   );
 };
