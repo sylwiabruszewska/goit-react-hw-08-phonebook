f21ea0b4d2f6d938f0ab7f335613fd256372107b
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-10-05 02:28:37 +0200

    fix modal close on button

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,9 +1,14 @@
 import { Overlay, StyledIconClose, BoxForButton } from './Modal.styled.js';
 import { Container, ContactDetails, ButtonIcon } from 'components';
 import { useModal } from 'hooks/useModal.js';
+import { useSelector } from 'react-redux';
+
+import { selectModalOpen } from 'redux/modal/selectors.js';
 
 export const Modal = () => {
   const { handleCloseModal } = useModal();
+
+  const ismodalopen = useSelector(selectModalOpen);
 
   const handleOverlayClick = event => {
     if (event.target === event.currentTarget) {
@@ -11,11 +16,16 @@ export const Modal = () => {
     }
   };
 
+  const handleCloseButton = () => {
+    console.log(ismodalopen);
+    handleCloseModal();
+  };
+
   return (
     <Overlay onClick={handleOverlayClick}>
       <Container>
         <BoxForButton>
-          <ButtonIcon onClick={handleCloseModal}>
+          <ButtonIcon type="button" handler={handleCloseButton}>
             <StyledIconClose />
           </ButtonIcon>
         </BoxForButton>
